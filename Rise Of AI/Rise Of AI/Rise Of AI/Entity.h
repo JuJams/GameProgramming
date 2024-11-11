366128eea8a6a413caafda295ea29ada19ceb466
JuJams <sk10173@nyu.edu>
2024-11-11 00:24:03 -0500

    Update

--- Rise Of AI/Rise Of AI/Rise Of AI/Entity.h
+++ Rise Of AI/Rise Of AI/Rise Of AI/Entity.h
@@ -1,3 +1,13 @@
+/**
+* Author: Sanjana Chowdary
+* Assignment: Rise of the AI
+* Date due: 2024-11-09, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #ifndef ENTITY_H
 #define ENTITY_H
 
